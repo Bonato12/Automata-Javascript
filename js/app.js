f988f9fa76674f9add311d4a1b21de36ce1f550e
Sebastian Bonato <sebabonato12@gmail.com>
2019-02-11 23:58:35 -0300

    agregue tabla

--- js/app.js
+++ js/app.js
@@ -71,6 +71,8 @@ function Principal(caracter){
     }else{
             cadenaNoValida();
     }
+    tabla=[[1,8,8,4,8,8],[1,2,2,8,5,8],[3,8,8,8,8,8],[3,8,8,8,5,8],[1,8,8,8,8,8],[6,8,8,7,8,8],[6,8,8,8,8,8],[6,8,8,8,8,8],[8,8,8,8,8,8]];
+    console.log(tabla);
 }
 
 function cadenaValida(){
