398154a8f63d5d4dcbc3e679143094079bfb6883
Sebastian Bonato <sebabonato12@gmail.com>
2019-07-23 19:47:50 -0300

    Last Commit -m

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var simbolo = " ";
+//Funcion que determina si un estado es valido o no
 function Control(caracter){
     if (caracter >=0 && caracter <= 9){
           simbolo = "Digito";
@@ -34,7 +35,7 @@ function headerTabla(){
       document.write('</table></center>');
 }
 
-
+//Muestra los estados en forma de tabla
 function bodyTabla(estadoAct,character,simbolo,estadoSig){
       document.write('<center><table width:"100%" border="4">');
       document.write('<tr align="center" style=\"color:white;\"><td width="180px" >' + estadoSig + '</td><td width="180px">' +  character + '</td><td width="180px">' + simbolo + '</td><td width="180px">' + estadoAct + '</td></tr>');
@@ -43,7 +44,7 @@ function bodyTabla(estadoAct,character,simbolo,estadoSig){
 }
 
 
-
+//Tabla de Transacciones que cree en base al AFD
 tabla=[[1,8,8,4,8,8],
        [1,2,2,8,5,8],
        [3,8,8,8,8,8],
@@ -54,6 +55,7 @@ tabla=[[1,8,8,4,8,8],
        [6,8,8,8,8,8],
        [8,8,8,8,8,8]]
 
+//Funcion que recorre el caracter ingresado y guarda el Estado segun el valor obtenido
 function Estado(caracter,tabla){
       var estadoAct = 0;
       for(var item in caracter){
@@ -64,13 +66,17 @@ function Estado(caracter,tabla){
 
        }
        if(estadoAct!=3 && estadoAct!=6){
+          //Se Ejecuta un Alert Si el estado actual es distinto de 3 y de 6 la cadena es Invalida
+
           alert("Cadena Invalida");
        }else {
+          //Se ejecuta un Alert Si el estado actual es igual a 3 y de 6 la cadena es Valida
           alert("Cadena Valida");
        }
 
 }
 
+//Funcion que se encarga de introducir la cadena y llamar a las funciones que cree arriba
 function Ejecutar(){
   var caracter = prompt("Ingrese una Cadena", "");
   headerTabla();
